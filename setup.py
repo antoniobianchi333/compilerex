578d3c6672ee295eeb9e992c7f5955bac29add42
Antonio Bianchi <antoniob@cs.ucsb.edu>
2020-06-22 01:40:27 -0400

    setup

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ class build(_build):
     def run(self):
         for f in os.listdir("compilerex/bin/"):
             os.chmod(os.path.join("compilerex/bin/", f), 0o777)
+        for f in os.listdir("scripts/bin/"):
+            os.chmod(os.path.join("scripts/bin/", f), 0o777)
 
 
 setup(
